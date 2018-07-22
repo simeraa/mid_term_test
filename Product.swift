887c31c8df43947bdfec672b24a5d55223ee8020
simeraa <simerpreet260719@gmail.com>
2018-07-22 18:29:06 -0400

    second

---
+++ Product.swift
@@ -0,0 +1,53 @@
+//
+//  Product.swift
+//  mid_term
+//
+//  Created by MacStudent on 2018-07-21.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Product
+{
+    var Pid : Int?
+    var Pname : String?
+    var Price  : Float?
+    var Quantity : Int?
+{
+    
+    init()
+    {
+        Pid = 1
+        Pname = ""
+        Price = 0.0
+        Quantity = 0
+    }
+    
+    init(Pid : Int , Pname : String , Price : Float ,Quantity : Float)
+    {
+        self.Pid =  Pid
+        self.Pname = Pname
+        self.Price = Price
+        self.Quantity = Quantity
+        }
+    
+    
+    
+    func setdata(Pid : Int , Pname : String , Price : Float ,Quantity : Float)
+    {
+        self.Pid =  Pid
+        self.Pname = Pname
+        self.Price = Price
+        self.Quantity = Quantity
+        
+    }
+    
+    func display1()
+    {
+        print("id: \(self.Pid!)")
+        print("Pname : \(self.Pname!)")
+        print("price :\(self.Price!)")
+        print("Quantity : \(self.Quantity!)")
+    }
+}
